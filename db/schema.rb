0fa0218476d15019b5d7eca010379027847751d2
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-08-08 14:06:55 +0000

    [add]genremodel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_08_140224) do
+ActiveRecord::Schema.define(version: 2021_08_08_140601) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2021_08_08_140224) do
   create_table "favorites", force: :cascade do |t|
     t.integer "anime_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
