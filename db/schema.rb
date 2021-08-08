6a9b6074beaca22a3a2d45209c9dffe7478fc46d
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-08-08 14:12:23 +0000

    [add]messagemodel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_08_140601) do
+ActiveRecord::Schema.define(version: 2021_08_08_141059) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 2021_08_08_140601) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "message_content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
