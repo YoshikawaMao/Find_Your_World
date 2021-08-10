ba5f3d59510665a949e39ea9b5ec868e857e3367
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-08-10 15:13:58 +0000

    [add]genre機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,25 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'homes#top'
+  get "top" => "homes#top"
+
+  namespace :admin do
+    resources :animes
+    resources :comments
+    resources :voices
+    resources :messages
+    resources :genres
+  end
+
+  namespace :user do
+    resources :animes
+    resources :comments
+    resources :favorites
+    resources :voices
+    resources :messages
+  end
+
+
 end
