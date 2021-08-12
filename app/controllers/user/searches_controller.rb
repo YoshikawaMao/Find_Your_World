37115c0d09a3745d2c383b3d2c4722a6aef0d815
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-08-12 12:49:56 +0000

    [add]user側のジャンル検索機能

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -1,8 +1,9 @@
 class User::SearchesController < ApplicationController
 
   def search
-    # ここでジャンルid受け取る
-    @content=params["search"]["value"]
-    @records=Anime.search_for(@content)
+    @value = params["search"]["value"]
+    @datas = Anime.where(genre_id: @value)
+    @genres = Genre.all
   end
+
 end
--- app/models/anime.rb
+++ app/models/anime.rb
@@ -6,8 +6,4 @@ class Anime < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  def self.search_for(content)
-    Item.where('name LIKE ?', '%'+content+'%').or(Item.where(genre_id: content))
-  end
-
 end
