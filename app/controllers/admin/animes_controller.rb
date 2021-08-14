671aadeb7d89d8d31edef9303ac6c5981658a454
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-08-14 15:07:20 +0000

    [add]a_animes/showページ

--- app/controllers/admin/animes_controller.rb
+++ app/controllers/admin/animes_controller.rb
@@ -7,17 +7,22 @@ class Admin::AnimesController < ApplicationController
     @genres = Genre.all
   end
 
-  # アニメのurl
-  def create
-    @anime = Anime.new(anime_params)
-    if @anime.save
-      redirect_to admin_animes_path
-    else
-      @animes = Anime.all
-      render :index
-    end
-  end
 
+  def show
+    @anime = Anime.find(params[:id])
+  end
+  # アニメのurl 別に作った方がいいかも
+  # def create
+  #   @anime = Anime.new(anime_params)
+  #   if @anime.save
+  #     redirect_to admin_animes_path
+  #   else
+  #     @animes = Anime.all
+  #     render :index
+  #   end
+  # end
+
+  # アニメのtitle
   def destroy
     @anime = Anime.find(params[:id])
     @anime.destroy
@@ -25,6 +30,6 @@ class Admin::AnimesController < ApplicationController
   end
 
   def anime_params
-    params.require(:anime).permit(:title, :anime_url)
+    params.require(:anime).permit(:title)
   end
 end
