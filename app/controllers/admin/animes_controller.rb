ef3b3d90dfb68aab97e740bac0037a1d6563665b
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-08-14 13:46:25 +0000

    [add]a_animes/indexページ

--- app/controllers/admin/animes_controller.rb
+++ app/controllers/admin/animes_controller.rb
@@ -11,14 +11,20 @@ class Admin::AnimesController < ApplicationController
   def create
     @anime = Anime.new(anime_params)
     if @anime.save
-       redirect_to admin_animes_path
+      redirect_to admin_animes_path
     else
       @animes = Anime.all
       render :index
     end
   end
 
+  def destroy
+    @anime = Anime.find(params[:id])
+    @anime.destroy
+    redirect_to admin_animes_path
+  end
+
   def anime_params
-    params.require(:anime).permit(:user_id, :genre_id, :title, :anime_url)
+    params.require(:anime).permit(:title, :anime_url)
   end
 end
