3611dfd0c6aac9a463411a3b2336da18928fb09f
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-08-18 07:08:30 +0000

    [add]いいね機能非同期通信化

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -3,13 +3,13 @@ class User::FavoritesController < ApplicationController
     @anime = Anime.find(params[:anime_id])
     @favorite = current_user.favorites.new(anime_id: @anime.id)
     @favorite.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @anime = Anime.find(params[:anime_id])
     @favorite = current_user.favorites.find_by(anime_id: @anime.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 end
