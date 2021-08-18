74ab13d092c6fe1c30f3cc865243af69e9b6ff4d
YoshikawaMao <lapinnoir2568stella@gmail.com>
2021-08-18 05:36:40 +0000

    [add]ログイン後の遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,29 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
- #ログイン後の遷移画面(管理者&顧客)
+ #ログイン後の遷移画面(管理者&user)
   def after_sign_in_path_for(resource)
     case resource
     when Admin
-    admin_animes_path
+      admin_animes_path
     when User
       user_animes_path
     end
   end
 
-  #ログアウト後の遷移画面(管理者&顧客)
-  def after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :admin
+  #ログアウト後の遷移画面(管理者&user)
+  def after_sign_out_path_for(resource)
+    if resource == :admin
       new_admin_session_path
     else
       root_path
     end
+    # case resource
+    # when Admin
+    #   new_admin_session_path
+    # when User
+    #   root_path
+    # end
   end
 
 
